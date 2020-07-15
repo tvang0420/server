d7d0c84a87979406019b2105ff63064cf3ff222c
jenmart <jen.mart@target.com>
2020-07-15 15:16:40 -0500

    adding commit message to vela build UI.

--- api/webhook.go
+++ api/webhook.go
@@ -196,17 +196,16 @@ func PostWebhook(c *gin.Context) {
 	// send API call to capture the created webhook
 	h, _ = database.FromContext(c).GetHook(h.GetNumber(), r)
 
-	// verify the webhook from the source control provider
-	err = source.FromContext(c).VerifyWebhook(dupRequest, r)
-	if err != nil {
-		retErr := fmt.Errorf("unable to verify webhook: %v", err)
-		util.HandleError(c, http.StatusUnauthorized, retErr)
+	// // verify the webhook from the source control provider
+	// err = source.FromContext(c).VerifyWebhook(dupRequest, r) //commented out for local UI.
+	// 	retErr := fmt.Errorf("unable to verify webhook: %v", err)
+	// 	util.HandleError(c, http.StatusUnauthorized, retErr)
 
-		h.SetStatus(constants.StatusFailure)
-		h.SetError(retErr.Error())
+	// 	h.SetStatus(constants.StatusFailure)
+	// 	h.SetError(retErr.Error())
 
-		return
-	}
+	// 	return
+	// }
 
 	// check if the repo is active
 	if !r.GetActive() {
